fb598f0b8336afaa75af7f690045f011adcc7090
Sachintha_Dissanayaka <95671286+SDDISSANAYAKA@users.noreply.github.com>
2024-07-12 16:31:19 +0530

    Commit Create form

--- ASPNET_MVC_APP/Controllers/HelloWorldController.cs
+++ ASPNET_MVC_APP/Controllers/HelloWorldController.cs
@@ -5,16 +5,27 @@ namespace ASPNET_MVC_APP.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel()
-            { Name = "Sif", Age = 2};
-            return View(doggo);
+            //DogViewModel doggo = new DogViewModel()
+            //{ Name = "Sif", Age = 2 };
+            //return View(doggo);
+
+            return View(dogs);
         }
 
         public IActionResult Create()
         {
+            var dogVm = new DogViewModel();
             return View();
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
         }
 
         public String Hello()
